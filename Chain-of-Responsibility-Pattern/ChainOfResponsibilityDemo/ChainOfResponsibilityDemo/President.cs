3a63105dc161dad6f1be5fa73899edcee77973ca
Bang <sharleen.bang@bearingpoint.com>
2021-12-15 10:23:02 +0000

    Added Chain-of-Responsibility Code Demo & Powerpoint

---
+++ Chain-of-Responsibility-Pattern/ChainOfResponsibilityDemo/ChainOfResponsibilityDemo/President.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityDemo
+{
+    public class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Quantity < 100000.0)
+            {
+                Console.WriteLine("{0} approved of request ID: {1} \n", this.GetType().Name, purchase.Id);
+            }
+            else {
+                Console.WriteLine("Request {0} requires a meeting for discussion. \n", purchase.Id);
+            }
+
+        }
+    }
+}
---
+++ Chain-of-Responsibility-Pattern/ChainOfResponsibilityDemo/ChainOfResponsibilityDemo/Purchase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityDemo
+{
+    public class Purchase
+    {
+        int id;
+        double quantity;
+        string description;
+
+        public Purchase(int id, double quantity, string description) {
+            this.id = id;
+            this.quantity = quantity;
+            this.description = description;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public string Descrption
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+    }
+}
