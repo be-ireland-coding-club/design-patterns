ede2ed5b42fac6a88db87d3a4ffeb7e1c9135694
Bang <sharleen.bang@bearingpoint.com>
2021-12-15 10:29:53 +0000

    Added Composite Code Demo & Powerpoint

---
+++ Composite-Pattern/CompositeDemo/CompositeDemo/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeDemo
+{
+    public class Manager : IEmployee
+    {
+        private readonly string name;
+        private readonly long employeeId;
+        private readonly string position;
+
+        public Manager(long employeeId, string name, string position) {
+            this.employeeId = employeeId;
+            this.name = name;
+            this.position = position;
+        }
+        public void GetData()
+        {
+            Console.WriteLine("EmployeeId: {0} | Name: {1} | Position: {2} \n", this.employeeId, this.name, this.position);
+        }
+    }
+}
